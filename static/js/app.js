97dcf6dd408c14a6b3aada14674681d5b1b7adbc
MV Admin <mvadmin@MVCC-MACMINI-86.local>
2018-03-28 19:11:47 -0700

    Maybe fixed some stuff

--- static/js/app.js
+++ static/js/app.js
@@ -13,7 +13,7 @@ firebase.initializeApp(config);
 var username = "";
 var database = firebase.database();
 var messages = [];
-var messageId = null;
+var messageId = 0;
 database.ref("messageId").once("value").then(function(snapshot) {
 	messageId = snapshot.val();
 });
@@ -22,6 +22,7 @@ database.ref("messages/").once("value").then(function(snapshot) {
 	messages = snapshot.val();
 });
 
+
 function usernameEntered() {
 	if (event.keyCode == 13) {
 		username = $("#username").val();
@@ -29,7 +30,7 @@ function usernameEntered() {
 		$("#usernameEnter").remove();
 		$("button").remove();
 		$("br").remove();
-		$("body").html("Enter your message here: <input id = \"userMessage\" onkeydown = \"sendMessage()\"></input></br>");
+		$("body").html("Enter your message here: <input id = \"userMessage\" onkeydown = \"sendMessage()\"></input></br><textarea id = \"theTextArea\" rows = \"50\" cols = \"100\"></textarea></br>");
 		
 		// for (var i = 0; i < messageId; i++) {
 		// 	database.ref("messages/" + messageId).once("value").then(function(snapshot) {
@@ -38,10 +39,23 @@ function usernameEntered() {
 		// 	});
 		// }
 
+		if (messageId == null) {
+			messageId = 0;
+		}
+
+		if (messages == null) {
+			messages = ["ADMIN: Welcome to the start of a new chat!"];
+			database.ref("messages/" + messageId).set(username + ": " + userMessage);
+			messageId++;
+			database.ref("messageId").set(messageId);
+		}
+
+
 		for (var i = 0; i < messages.length; i++) {
-			$("body").append("</br>" + messages[i]);
+			$("#theTextArea").append("\n" + messages[i]);
 			console.log("debug - " + messages[i]);
 		}
+			
 
 		console.log(messages);
 
@@ -52,7 +66,7 @@ function usernameEntered() {
 }
 
 function sendMessage() {
-	if (event.keyCode == 13) {
+	if (event.keyCode == 13 && $("#userMessage").val() != "" && $("#userMessage").val() != " ") {
 		var userMessage = $("#userMessage").val();
 		console.log(userMessage);
 		$("#userMessage").val("");
@@ -62,7 +76,20 @@ function sendMessage() {
 		});
 		messageId++;
 		database.ref("messageId").set(messageId);
-		$("body").append("</br>" + messages[messageId - 1])
+		$("#theTextArea").html("");
+		//$("#theTextArea").append("\n" + messages[messageId - 1])
+		for (var i = 0; i < messages.length; i++) {
+			$("#theTextArea").append("\n" + messages[i]);
+			console.log("debug - " + messages[i]);
+		}
+		$("#theTextArea").scrollTop = $("#theTextArea").scrollHeight;
 	}
-	
 }
+
+database.ref("messages/").on("value", function(snapshot) {
+	$("#theTextArea").html("");
+	for (var i = 0; i < messages.length; i++) {
+		$("#theTextArea").append("\n" + messages[i]);
+		console.log("debug - " + messages[i]);
+	}
+})
